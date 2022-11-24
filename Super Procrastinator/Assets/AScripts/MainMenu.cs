29dd70612e0468a85eb249110745def316a4db4f
B00145512 <B00145512@mytudublin.ie>
2022-11-24 20:41:44 +0000

    AlphaFinished

---
+++ Super Procrastinator/Assets/AScripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private ScoreManager theScoreManager;
+
+   void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+    }
+
+    public void Update()
+    {
+    bool restart = Input.GetKeyDown(KeyCode.R);
+     if (restart)
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            theScoreManager.scoreCount = 0;
+            theScoreManager.scoreIncrease = true;
+        }
+    }
+}
+
+        
+        
+
